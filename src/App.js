f8e54eb092dcaedbcc9f3bfd644abedf959c8ebf
Tom Kim <babooteng2@gmail.com>
2021-11-16 18:17:04 +0900

    Coin Code Challange

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { useEffect, useState } from "react";
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [coinInfo, setCoinInfo] = useState([]);
+  const saveCoinInfo = (source) => {
+    setCoinInfo([
+      source.name,
+      source.symbol,
+      source.quotes.USD.price,
+      source.last_updated,
+    ]);
+  };
+  const onChange = (e) => {
+    setAmount(e.target.value);
+  };
+  const onSelect = (e) => {
+    saveCoinInfo(JSON.parse(e.target.value));
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
+        saveCoinInfo(json[0]);
         setLoading(false);
       });
   }, []);
@@ -16,13 +34,38 @@ function App() {
       {loading ? (
         <strong>loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option value={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <>
+          <h2>Dollor to {coinInfo[0]}</h2>
+          <div>LastUpdated at : {coinInfo[3]}</div>
+          <hr />
+          <div>
+            <label htmlFor="money">
+              How much Dollar do you want exchange to {coinInfo[0]}?
+            </label>
+          </div>
+          <input
+            id="money"
+            type="number"
+            placeholder="input your Dollar amount"
+            value={amount}
+            onChange={onChange}
+          />
+          <div>
+            <select onChange={onSelect}>
+              {coins.map((coin) => (
+                <option key={coin.id} value={JSON.stringify(coin)}>
+                  {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                </option>
+              ))}
+            </select>
+            <hr />
+            <div>
+              <strong>
+                ${amount} = {amount / coinInfo[2]} {coinInfo[1]}
+              </strong>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
